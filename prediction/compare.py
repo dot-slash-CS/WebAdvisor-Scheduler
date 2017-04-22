c4bad1798ff5de756f1023eb20ced805c44b642e
Wolfgang C. Strack <wolfgangcstrack@gmail.com>
2017-04-21 18:20:20 -0700

    Update compare.py with a MeetingWrapper class to make comparing overlapping meetings easier

--- prediction/compare.py
+++ prediction/compare.py
@@ -4,10 +4,19 @@ import datetime
 # CONTAINER FUNCTION
 ##########################
 
+# Only for use in compare module
+class MeetingWrapper:
+    """Wraps a meeting object and adds a 'day' field"""
+    def __init__(self, meeting, day):
+        self.meeting = meeting # the meeting object to be wrapped
+        self.day     = day     # day of the week the meeting takes place on
+
 # Takes a meeting object (LogicComm's version) as a parameter
+#
 # Returns a list of meeting objects that represent the one meeting parameter
 # except split into different days of the week based on meeting.recurrence
 def split_meeting(meeting):
+    # TODO
     pass
 
 # Returns list of pairs of meetings that overlap
@@ -16,6 +25,7 @@ def overlapped_meeting_times(section1, section2):
     pass
 
 # Takes a list of pairs of meeting objects whose times overlap
+#
 # Returns true if ALL meetings that are overlapping can be resolved based on
 # location and duration of overlap
 def can_be_resolved(omts):
